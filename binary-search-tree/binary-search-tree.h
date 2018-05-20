0f2be630e6afbd3f28c942802c2395b7b41ed159
Cristiane Mayara <cris.mayara@gmail.com>
2018-05-20 20:38:20 -0300

    Creating a binary search tree

---
+++ binary-search-tree/binary-search-tree.h
@@ -0,0 +1,10 @@
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+typedef struct node {
+    int value;
+    struct node *left;
+    struct node *right;
+} Tree;
+
+#endif // BINARY_SEARCH_TREE_H
---
+++ binary-search-tree/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary-search-tree.h"
+
+int main() {
+  Tree* t = NULL;
+}
